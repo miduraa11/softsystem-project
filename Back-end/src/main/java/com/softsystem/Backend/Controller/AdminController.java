91bc79d8391bd9368d4a1157d5aa76deb33f25ff
URSIDAE97 <mateusz.m97@gmail.com>
2018-08-07 11:15:54 +0200

    Controller rework

--- Back-end/src/main/java/com/softsystem/Backend/Controller/AdminController.java
+++ Back-end/src/main/java/com/softsystem/Backend/Controller/AdminController.java
@@ -3,6 +3,6 @@ package com.softsystem.Backend.Controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MemberController {
-
+public class AdminController {
+    
 }
