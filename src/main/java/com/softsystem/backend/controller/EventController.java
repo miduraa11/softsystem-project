6320f879a26ff1fc2998681e7c79fcfcad5803ad
Adam Midura <amidura@softsystem.pl>
2018-08-30 14:34:21 +0200

    one more time

--- src/main/java/com/softsystem/backend/controller/EventController.java
+++ src/main/java/com/softsystem/backend/controller/EventController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class EventController {
 
